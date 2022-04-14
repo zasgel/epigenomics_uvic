b4e7a5c0eeba30c1eb398f79eb33622f4604a6b9
zasgel <103458244+zasgel@users.noreply.github.com>
2022-04-14 22:22:29 +0200

    Add files via upload

---
+++ get.distance.py
@@ -0,0 +1,42 @@
+
+#!/usr/bin/env python
+
+#************
+# LIBRARIES *
+#************
+
+import sys
+from optparse import OptionParser
+
+#*****************
+# OPTION PARSING *
+#*****************
+
+parser = OptionParser()
+parser.add_option("-i", "--input", dest="input")
+parser.add_option("-s", "--start", dest="start")
+options, args = parser.parse_args()
+
+open_input = open(options.input)
+enhancer_start = int(float(options.start.strip()))
+
+#********
+# BEGIN *
+#********
+
+
+x = 1000000 # set maximum distance to 1 Mb
+selectedGene = "" # initialize the gene as empty
+selectedGeneStart = 0 # initialize the start coordinate of the gene as empty
+
+for line in open_input.readlines(): # for each line in the input file
+        gene, y = line.strip().split('\t') # split the line into two columns based on a tab 
+        position = int(y) # define a variable called position that correspond to the integer of the start of the gene
+        value = abs(position - enhancer_start) # compute the absolute value of the difference between position and enhancer_start
+
+        if value < x: # if this absolute value is lower than maximumDistance
+                x = value # this value will now be your current maximumDistance
+                selectedGene = gene # save gene as selectedGene
+                selectedGeneStart = position # save position as selectedGeneStart
+
+print "\t".join([selectedGene, str(selectedGeneStart), str(x)])
